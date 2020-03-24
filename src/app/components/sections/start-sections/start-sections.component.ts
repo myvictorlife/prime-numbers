839761e23986d2983d2e7ba6d148c975f13f5f6a
Victor Cesar <victorcmggg@gmail.com>
2020-03-24 18:44:08 +0000

    Add: Sieve of Eratosthenes is one of the most efficient ways to find all primes smaller

--- src/app/components/sections/start-sections/start-sections.component.ts
+++ src/app/components/sections/start-sections/start-sections.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import { increment } from '../../../store/store';
+import calculatePrimeArray from '../../../util/prime-numbers'
 @Component({
   selector: 'app-start-sections',
   templateUrl: './start-sections.component.html',
@@ -10,6 +11,20 @@ import { increment } from '../../../store/store';
 export class StartSectionsComponent implements OnInit {
 
   public counter$: Observable<any>;
+  sumOfFoundPrimes = 0;
+
+  // ******************************
+  index = 1000;
+  arrayLastPrimeNumber = 1000;
+  primeNumberFound;
+
+  controlPrimeNumbers = {
+    calculationTime: null,
+    sumOfFoundPrimes: 0,
+    primesNumberFound: [],
+    notifications: []
+  };
+  // ******************************
 
   constructor(private store: Store<{ counterReducer: number }>) { }
 
@@ -18,10 +33,39 @@ export class StartSectionsComponent implements OnInit {
     this.counter$ = this.store.pipe(
       select('counterReducer')
     );
+    this.calculate();
   }
 
   startCalculation() {
     this.store.dispatch(increment());
+    this.calculate();
+  }
+
+
+
+
+
+  calculate() {
+    let index = 1000;
+    let array = [];
+
+    while(true) {
+      array = calculatePrimeArray(index);
+      const last1000Prime = array[this.arrayLastPrimeNumber - 1];
+
+      if (last1000Prime) {
+        break;
+      }
+      index += index;
+    }
+
+    this.sumOfFoundPrimes = array.reduce((sum, value) => sum + value, 0);
+    this.primeNumberFound = array[this.arrayLastPrimeNumber - 1];
+    this.arrayLastPrimeNumber += 1000;
+
+    this.controlPrimeNumbers.sumOfFoundPrimes = this.sumOfFoundPrimes;
+    this.controlPrimeNumbers.primesNumberFound.push(this.primeNumberFound);
+    this.controlPrimeNumbers.notifications.push(`Prime ${this.primeNumberFound} was found after 00:00:00`);
   }
 
 }
